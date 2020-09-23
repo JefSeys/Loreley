6ba7f39b67ff1f454299964c500cbb192e24ecc7
Jef Seys <jef.seys.y0431@student.hogent.be>
2020-09-23 22:45:34 +0200

    Opmaak

---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+var index = 1;
+
+function changeGallery(n){
+ showGallery(index+=n)
+}
+
+timer = setTimeout(() => showGallery( index += 1), 8000);
+
+function showGallery(n){
+    var i;
+    var gallery = document.getElementsByClassName("galleryImg");
+    console.log(gallery)
+    if (n > gallery.length) {index = 1}
+    if (n < 1) {index = gallery.length}
+    for (i = 0; i < gallery.length; i++) {
+        gallery[i].style.display = "none";
+    }
+    gallery[index-1].style.display = "block";
+
+    clearTimeout(timer);
+    
+    timer = setTimeout(() => showGallery( index += 1), 8000);
+
+    timer;
+}
+
+
+timer;
